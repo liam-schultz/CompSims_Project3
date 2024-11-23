2a1d80e86751e410acbe714f4ed78c48e43741e0
Liam Schultz <lschult7@uwo.ca>
2024-11-23 16:41:56 -0500

    added an event to terminate solution and wrote the solve_ivp call

--- Schultz_Liam_Project3.py
+++ Schultz_Liam_Project3.py
@@ -1,9 +1,18 @@
 import numpy as np
+import scipy as sp
 
 def drhodr_dmdr(r, state):
     #state = [rho, m]
     rho = state[0]
     m = state[1]
-    drho = (3*m*rho*np.sqrt(1+np.cbrt(rho)**2))/(np.cbrt(rho)**2*r**2)
+    drho = -(3*m*rho*np.sqrt(1+np.cbrt(rho)**2))/(np.cbrt(rho)**2*r**2)
     dm = r**2*rho
-    return [drho, m]
+    return [drho, dm]
+
+def zero_density(r, state):
+    zero_density.terminal = True
+    return state[0]
+
+
+sol = sp.integrate.solve_ivp(drhodr_dmdr, (1e-100, 1e300), [10, 0], events=zero_density)
+print(sol)
